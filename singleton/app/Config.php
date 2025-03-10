bbd5d718f6bcba75a2640f305b98bf359ff25560
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-10 16:39:28 +0100

    feat: Implement Config class as a Singleton for configuration management

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,22 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config
+{
+    private static $settings;
+
+    private function __construct()
+    {
+        $this->settings = parse_ini_file(__DIR__ . "/../config.php");
+    }
+
+    public static function get()
+    {
+        if (is_null(self::$settings)) {
+            self::$settings = new Config();
+        }
+
+        return self::$settings;
+    }
+}
